cc81f7d8ae3ff24814ee9c595253294a9a7eaa66
jawad <jawadabbas680@gmail.com>
2025-09-12 04:42:27 +0500

    rush

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
-let CopyWebpackPlugin;
-if (isProduction) {
-  try {
-    CopyWebpackPlugin = require('copy-webpack-plugin');
-  } catch (e) {
-    // In production builds this should be installed; in dev we don't hard-require it
-  }
-}
 
 module.exports = {
   entry: './src/index.js',
@@ -43,35 +36,23 @@ module.exports = {
       },
     ],
   },
-  plugins: (() => {
-    const base = [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-      }),
-    ];
-    if (isProduction && CopyWebpackPlugin) {
-      base.push(
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-              from: path.resolve(__dirname, 'public'),
-              to: path.resolve(__dirname, 'dist'),
-              globOptions: {
-                ignore: ['**/index.html'],
-              },
-              noErrorOnMissing: true,
-            },
-            {
-              from: path.resolve(__dirname, 'public', 'logo1.png'),
-              to: path.resolve(__dirname, 'dist', 'favicon.ico'),
-              noErrorOnMissing: true,
-            },
-          ],
-        })
-      );
-    }
-    return base;
-  })(),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '.',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
